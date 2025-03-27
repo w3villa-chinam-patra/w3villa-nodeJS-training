16ff4950e0c609feb9bc823345c39713dc25cb2c
w3villa-chinam-patra <chinam.patra@w3villa.com>
2025-03-27 18:42:06 +0530

    create a calculator package

---
+++ CustomPackage/calculator/index.mjs
@@ -0,0 +1,6 @@
+import { add } from "./add.mjs";
+import { sub } from "./sub.mjs";
+import { div } from "./div.mjs";
+import { mul } from "./mul.mjs";
+
+export { add, sub, div, mul };
---
+++ CustomPackage/script.mjs
@@ -0,0 +1,6 @@
+import { add, sub, mul, div } from "./calculator/index.mjs";
+
+console.log(add(5, 6));
+console.log(sub(6, 5));
+console.log(mul(6, 5));
+console.log(div(6, 5));
